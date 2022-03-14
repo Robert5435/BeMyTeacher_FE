fea07d4b392ea846cb8c1472406a2b6d421c87ec
Robert5435 <robert.nitza5@gmail.com>
2022-03-14 14:20:12 +0200

    Props Mapped to database data

--- src/components/Profile/ProfileTutoringAds.js
+++ src/components/Profile/ProfileTutoringAds.js
@@ -1,15 +1,28 @@
 import Card from "../../ui/Card";
 import classes from "./ProfileTutoringAds.module.css"
 
-function ProfileTutoringAds(){
+function ProfileTutoringAds(props){
+
+    function handleDelete(){
+        fetch("https://localhost:5001/TutoringAds/"+props.id,
+        { 
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' } ,
+            credentials:'include',
+        });
+    }
+
+
+
+
     return <div className={classes.cards}> <Card>
-        <p className={classes.title}>Title</p>
+        <p className={classes.title}>{props.title}</p>
         <div className={classes.actions}>
-        <button>Delete</button>
+        <button onClick={handleDelete}>Delete</button>
         </div>
-        <span className={classes.subject}>Subject</span>
-        <span className={classes.location}>Location</span>
-        <p className={classes.price}>Price/Session lenght: 12 lei/ 120 min</p>
+        <span className={classes.subject}>{props.subject}</span>
+        <span className={classes.location}>{props.location}</span>
+        <p className={classes.price}>Price/Session lenght: {props.pricePerSession}Lei/ {props.sessionLenghtinMinutes} Min</p>
     </Card>
     </div>
     }
