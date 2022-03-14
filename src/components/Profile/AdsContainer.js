af5db8b5bbbae6cbdf21c8d6d5d2abc8dde12c8d
Robert5435 <robert.nitza5@gmail.com>
2022-03-14 14:19:34 +0200

    Mapping ADs to the specific props

--- src/components/Profile/AdsContainer.js
+++ src/components/Profile/AdsContainer.js
@@ -2,15 +2,27 @@ import Card from "../../ui/Card";
 import ProfileTutoringAds from "./ProfileTutoringAds";
 import classes from "./AdsContainer.module.css"
 
-function AdsContainer(){
+function AdsContainer(props){
+    console.log(props.ads)
+
 return <div className={classes.container}>
     <Card>
-    <ProfileTutoringAds/>
-    <ProfileTutoringAds/>
-    <ProfileTutoringAds/>
-    <ProfileTutoringAds/>
-    <ProfileTutoringAds/>
+    {props.ads.map((ad) =>(
+            
+            <ProfileTutoringAds 
+                key={ad.id}
+                id={ad.id}
+                image={ad.image}
+                title={ad.image}
+                subject={ad.subject}
+                location={ad.location}
+                pricePerSession={ad.pricePerSession}
+                sessionLenghtinMinutes={ad.sessionLenghtinMinutes}
+            />
+            
+    ))}
     </Card>
+
 </div>
 }
 
