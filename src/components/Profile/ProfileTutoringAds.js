1278272307aec56780743c33685e53020bdb0a28
Robert5435 <robert.nitza5@gmail.com>
2022-03-16 13:20:40 +0200

    reload after delete

--- src/components/Profile/ProfileTutoringAds.js
+++ src/components/Profile/ProfileTutoringAds.js
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+import { Redirect} from "react-router-dom";
 import Card from "../../ui/Card";
 import classes from "./ProfileTutoringAds.module.css"
 
 function ProfileTutoringAds(props){
+    console.log(props.pricePerSession, props.subject,props.location)
 
     function handleDelete(){
         fetch("https://localhost:5001/TutoringAds/"+props.id,
@@ -10,15 +13,16 @@ function ProfileTutoringAds(props){
             headers: { 'Content-Type': 'application/json' } ,
             credentials:'include',
         });
+        window.location.reload();
     }
 
+    var link = "/edit/" + props.id;
 
-
-
-    return <div className={classes.cards}> <Card>
+    return <div className={classes.cards}> 
+    <Card>
         <p className={classes.title}>{props.title}</p>
         <div className={classes.actions}>
-        <button onClick={handleDelete}>Delete</button>
+        <button onClick={handleDelete}>Delete</button><Link to={link} className="btn btn-primary">Edit</Link>
         </div>
         <span className={classes.subject}>{props.subject}</span>
         <span className={classes.location}>{props.location}</span>
