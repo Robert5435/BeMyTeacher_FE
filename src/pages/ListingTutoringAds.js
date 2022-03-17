fb755c6d4a11156bfaab97b69eb78cea0dc8bcba
Robert5435 <robert.nitza5@gmail.com>
2022-03-17 14:58:53 +0200

    Url concatenate for search

--- src/pages/ListingTutoringAds.js
+++ src/pages/ListingTutoringAds.js
@@ -7,54 +7,71 @@ import classes from "./ListingTutoringAds.module.css";
 
 function ListingTutoringAdsPage() {
 
+
+
+
     const [subject, setSubject] = useState();
     const [location, setLocation] = useState();
     const [isLoading, setIsLoading] = useState(true);
     const [loadedTutoringAds, setLoadedTutoringAds] = useState([]);
-    
+
     const optionsSubject = [
         { value: 1, label: 'Mathematics' },
         { value: 3, label: 'Computer Science' },
         { value: 4, label: 'English' },
         { value: 2, label: 'History' }
     ]
-    const optionsLocation=[
-        {value: 1, label:'Bucharest'},
-        {value: 2, label:'Brasov'},
-        {value: 3, label:'Arad'},
-        {value: 4, label:'Constanta'}
+    const optionsLocation = [
+        { value: 1, label: 'Bucharest' },
+        { value: 2, label: 'Brasov' },
+        { value: 3, label: 'Arad' },
+        { value: 4, label: 'Constanta' }
     ]
+
+    let params = (new URL(document.location)).searchParams;
+    let selectedSubjectId = params.get('selectedSubjectId');
+    let selectedLocationId = params.get('selectedLocationId');
+    let url = `https://localhost:5001/TutoringAds`;
+
+    if (selectedSubjectId) {
+        url = url + `?selectedSubjectId=${selectedSubjectId}`
+    }
+
     useEffect(() => {
         setIsLoading(true)
-        fetch("https://localhost:5001/TutoringAds").then(response => {
-            return response.json();
-        }).then(data => {
-            const tutoringAds = [];
-            for (const key in data) {
-                const tutoringAd = {
-                    id: key,
-                    ...data[key]
-                };
-                tutoringAds.push(tutoringAd);
-            }
-            setIsLoading(false);
-            setLoadedTutoringAds(tutoringAds); 
+
+        fetch(url)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                const tutoringAds = [];
+                for (const key in data) {
+                    const tutoringAd = {
+                        id: key,
+                        ...data[key]
+                    };
+                    tutoringAds.push(tutoringAd);
+                }
+                console.log(tutoringAds, "listing ads")
+                setIsLoading(false);
+                setLoadedTutoringAds(tutoringAds);
 
 
-        })
+            })
     }, []);
 
 
     function search(loadedTutoringAds) {
-        if(subject || location){
-            if(subject){
-            loadedTutoringAds = loadedTutoringAds.filter(a => a.subjectId == subject.value)
+        if (subject || location) {
+            if (subject) {
+                loadedTutoringAds = loadedTutoringAds.filter(a => a.subjectId == subject.value)
+            }
+
+            if (location) {
+                loadedTutoringAds = loadedTutoringAds.filter(a => a.locationId == location.value)
+            }
         }
-        
-        if(location){
-            loadedTutoringAds= loadedTutoringAds.filter(a => a.locationId == location.value)
-        }
-    }
         return loadedTutoringAds;
 
     }
