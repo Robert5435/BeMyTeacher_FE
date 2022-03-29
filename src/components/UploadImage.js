1ea4d4d288d5341b449851a300f1ce2bc069e3bf
Robert5435 <robert.nitza5@gmail.com>
2022-03-29 15:16:27 +0300

    Upload image Component Implemented

---
+++ src/components/UploadImage.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function UploadImage() {
+    const [selectedFile, setSelectedFile] = useState();
+    const [isFilePicked, setIsFilePicked] = useState(false);
+
+    const changeHandler = (event) => {
+        setSelectedFile(event.target.files[0]);
+        setIsFilePicked(true)
+    };
+
+    return (
+        <div >
+            <label htmlFor="file-upload" style={{
+                border: "1px solid #ccc",
+                display: "inline-block",
+                padding: "6px 12px",
+                cursor: "pointer"
+            }}>Upload Image</label>
+            <input type="file" id="file-upload" name="file" onChange={changeHandler} style={{ display: "none" }} />
+        </div>
+    )
+}
+export default UploadImage;
