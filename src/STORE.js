91b053d575ed9c68c78e3bd6a5394600bbfbb3db
Robert5435 <robert.nitza5@gmail.com>
2022-04-06 13:50:16 +0300

    Rating teacher and read only rating

--- src/STORE.js
+++ src/STORE.js
@@ -4,4 +4,5 @@ import {atom} from 'jotai'
 export const isModal = atom(false);
 export const isDeleting = atom(false);
 export const isEditProfileModal = atom(false);
+export const ratingValue = atom(0);
 export const user = atom({});
--- src/ui/StarsRating.js
+++ src/ui/StarsRating.js
@@ -2,30 +2,70 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
+import { useState } from 'react';
+import { ratingValue } from '../STORE';
+import { useAtom } from 'jotai';
+
 
 export default function StarsRating(props) {
-  const [value, setValue] = React.useState(2);
+    console.log(props,"values rating")
+    const [value, setValue] = useState(props.rating);
+    const [counter, setCounter] = useState(props.rateCounter)
+    const [valueRating, setValueRating] = useAtom(ratingValue);
 
-  return (
-    <Box
-      sx={{
-        '& > legend': { mt: 2 },
-      }}
-    >
-      <Typography component="legend">Rate teacher</Typography>
-      <Rating
-        name="simple-controlled"
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      />
-      {/* <Typography component="legend">Read only</Typography>
-      <Rating name="read-only" value={value} readOnly />
+    function eventHandler(newValue) {
+        console.log(newValue)
+        var formData = new FormData()
+        formData.append("rating", newValue);
+        formData.append("userId", props.userId);
+
+        fetch("https://localhost:5001/Users/Rate",
+            {
+                method: 'POST',
+                body: formData,
+                credentials: 'include',
+            });
+            setCounter(counter + 1);
+
+    }
+    if (props.readOnly) {
+        return (
+            <Box
+                sx={{
+                    '& > legend': { mt: 2 },
+                }}
+            >
+                <Typography component="legend">Read only</Typography>
+                <Rating name="read-only" defaultValue={value} readOnly />
+                <p>{counter} users have rated this teacher</p>
+
+            </Box>
+        );
+    } else {
+        return (
+            <Box
+                sx={{
+                    '& > legend': { mt: 2 },
+                }}
+            >
+                <Typography component="legend">Rate teacher</Typography>
+                <Rating
+                    name="simple-controlled"
+                    defaultValue={value}
+                    onChange={(event, newValue) => {
+                        setValue(newValue);
+                        eventHandler(newValue);
+                    }}
+                />
+                {/* 
       <Typography component="legend">Disabled</Typography>
       <Rating name="disabled" value={value} disabled />
       <Typography component="legend">No rating given</Typography>
       <Rating name="no-value" value={null} /> */}
-    </Box>
-  );
+            </Box>
+        );
+    }
+
+
+
 }
